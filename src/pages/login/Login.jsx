821baf5be197c0cd7bcad509050b609303094df7
laurenCho9 <lauren.choux@gmail.com>
2025-01-27 04:35:33 +0900

    로그인 폼 구현

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,52 @@
+import { useState } from "react";
 import styles from "./login.module.css";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLoginData = (e) => {
+    if (e.target.type === "email") {
+      setEmail(e.target.value);
+    } else if (e.target.type === "password") {
+      setPassword(e.target.value);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    // 페이지리로딩 제어
+    e.preventDefault();
+    console.log(email, password);
+  };
+
+  return (
+    <form className={styles.login_form} onSubmit={handleSubmit}>
+      <fieldset>
+        <legend>로그인</legend>
+        {/* email */}
+        <label htmlFor="myEmail">email: </label>
+        <input
+          type="email"
+          id="myEmail"
+          value={email}
+          onChange={handleLoginData}
+          required
+        />
+        {/* password */}
+        <label htmlFor="myPassword">password: </label>
+        <input
+          type="password"
+          id="myPassword"
+          value={password}
+          onChange={handleLoginData}
+          required
+        />
+
+        <button type="submit" className={styles.button}>
+          로그인
+        </button>
+      </fieldset>
+    </form>
+  );
 };
 export default Login;
